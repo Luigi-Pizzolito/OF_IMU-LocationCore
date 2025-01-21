87f3d3f163c684c1b63c8c3c957373417c67f863
Luigi Pizzolito <luigi.pizzolito@hotmail.com>
2025-01-22 00:04:47 +0800

    Updated espnow_recv test

--- LPLocationCore/test/test_espnow_recv/main.cpp
+++ LPLocationCore/test/test_espnow_recv/main.cpp
@@ -1,41 +1,50 @@
 #include <Arduino.h>
 
 #include <WiFi.h>
-#include <esp_now.h>
+#include <esp_wifi.h>
+#include <QuickEspNow.h>
 
-// callback when data is received
-void OnDataRecv(const uint8_t *mac, const uint8_t *data, int len)
-{
-    Serial.print("Last Packet Recv Data: ");
-    for (int i = 0; i < len; i++)
-    {
-        Serial.print((char)data[i]);
-    }
+#include <LiteLED.h>
+LiteLED led(LED_STRIP_WS2812, 0);
+static const crgb_t L_GREEN = 0xff0000;
+static const crgb_t L_RED = 0x00ff00;
+static const crgb_t L_BLUE = 0x0000ff;
+
+void dataReceived (uint8_t* address, uint8_t* data, uint8_t len, signed int rssi, bool broadcast) {
+    // Serial.println(millis());
+    // Serial.print("Received: ");
+    // Serial.printf("%.*s\n", len, data);
+    // Serial.printf("RSSI: %d dBm\n", rssi);
+    // Serial.printf("From: " MACSTR "\n", MAC2STR (address));
+    // Serial.printf("%s\n", broadcast ? "Broadcast" : "Unicast");
+    // Serial.println();
+    led.brightness(255,1);
+    Serial.printf("%d %.*s\n", millis(), len, data);
     Serial.println();
+    led.brightness(0,1);
 }
 
-void setup()
-{
+void setup () {
+    led.begin(21,1);
+    led.brightness(30);
+    led.setPixel(0,L_GREEN,1);
+
     Serial.begin(115200);
     delay(2000);
-    Serial.println("ESPNow Receiver");
 
-    // Set device as a Wi-Fi Station
     WiFi.mode(WIFI_STA);
-    // WiFi.disconnect();
+    WiFi.disconnect();
+    delay(100);
 
-    // Init ESP-NOW
-    if (esp_now_init() != ESP_OK)
-    {
-        Serial.println("Error initializing ESP-NOW");
-        return;
-    }
+    quickEspNow.onDataRcvd(dataReceived);
+    quickEspNow.begin(1);
+    delay(100);
 
-    // Once ESPNow is successfully Init, we will register for recv CB to
-    // get recv packer info
-    esp_now_register_recv_cb(OnDataRecv);
+    Serial.println("Ready to receive data on channel 1");
+    led.brightness(0,1);
+    led.setPixel(0,L_BLUE);
 }
 
-void loop()
-{
+void loop () {
+    delay(100);
 }
